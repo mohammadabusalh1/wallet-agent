f837ce82c2f5a91b6755108d63f6eae80bd74159
ameer017 <rajiabdullahi907@gmail.com>
2025-05-20 18:50:31 +0100

    folder 🌴 updated

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// test route
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
